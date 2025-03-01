8d1fab1dc8dc7e3cd9166b6f7f47fce1135f0233
Abhishek1743 <abhisheksagar1113@gmail.com>
2025-03-01 19:48:51 +0530

    Demonstrated Dependency Injection

--- src/main/java/com/example/SpringConcept/SpringConceptApplication.java
+++ src/main/java/com/example/SpringConcept/SpringConceptApplication.java
@@ -1,6 +1,7 @@
 package com.example.SpringConcept;
 
 import com.example.SpringConcept.component.DemoBean;
+import com.example.SpringConcept.component.EmployeeBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot. SpringApplication;
@@ -12,11 +13,13 @@ public class SpringConceptApplication {
 	public static final Logger logger = LoggerFactory.getLogger(SpringConceptApplication.class);
 
 	public static void main(String[] args) {
-		logger.debug("Welcome to Spring Concept");
+		logger.debug("Welcome to Spring Concept Demo");
 		ApplicationContext context = SpringApplication.run(SpringConceptApplication.class, args);
 		logger.debug("Checking Context: {}", context.getBean(DemoBean.class));
 		logger.debug("\n*** Example Using @Autowire annotation on property ***");
-
-	}
-
+		EmployeeBean employeeBean = context.getBean(EmployeeBean.class);
+		employeeBean.setEid(104);
+		employeeBean.setEname("Spring Framework Guru");
+		employeeBean.showEmployeeDetails();
+}
 }
